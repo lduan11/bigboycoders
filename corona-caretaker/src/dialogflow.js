32b994bd12ffb76484f83cec0181426c7e3132b9
Luke Duan <lduan11@live.unc.edu>
2020-06-12 20:49:37 -0400

    fixed janky indentations

--- corona-caretaker/src/dialogflow.js
+++ corona-caretaker/src/dialogflow.js
@@ -2,32 +2,32 @@ const dialogflow = require("dialogflow");
 const uuid = require("uuid");
 
 async function runSample(projectId = "1") {
-  const sessionId = uuid.v4();
+    const sessionId = uuid.v4();
 
-  const sessionClient = new dialogflow.SessionsClient();
-  const sessionPath = sessionClient.projectAgentSessionPath(
-    projectId,
-    sessionId
-  );
+    const sessionClient = new dialogflow.SessionsClient();
+    const sessionPath = sessionClient.projectAgentSessionPath(
+        projectId,
+        sessionId
+    );
 
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: "Hello",
-        languageCode: "en-US"
-      }
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: "Hello",
+                languageCode: "en-US"
+            }
+        }
+    };
+
+    const responses = await sessionClient.detectIntent(request);
+    console.log("Detected Intent");
+    const result = responses[0].queryResult;
+    console.log(` Query: ${result.queryText}`);
+    console.log(` Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+        console.log(` Intent: ${result.intent.displayName}`);
+    } else {
+        console.log(` No intent matched.`);
     }
-  };
-
-  const responses = await sessionClient.detectIntent(request);
-  console.log("Detected Intent");
-  const result = responses[0].queryResult;
-  console.log(` Query: ${result.queryText}`);
-  console.log(` Response: ${result.fulfillmentText}`);
-  if (result.intent) {
-    console.log(` Intent: ${result.intent.displayName}`);
-  } else {
-    console.log(` No intent matched.`);
-  }
 }
